0c4076962c1e5ef494a35b0adca0952b055c89f5
Paulo <paulohsantosrocha@gmail.com>
2024-09-20 10:27:37 -0300

    adiociona html

---
+++ public/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const tabelaObreiros = document.querySelector('#tabelaObreiros tbody')
+  const formObreiro = document.querySelector('formObreiro')
+  const resetPagamentosBnt = document.getElementById('resetPagamentos')
+})
